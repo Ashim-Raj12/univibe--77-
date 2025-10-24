7821d78df129cd9e3370b1d0e3eafa288b3eda8c
Ashim Raj <programwithashim@gmail.com>
2025-10-25 04:56:04 +0530

    Code break fix

---
+++ pages/CreateEventPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CreateEventPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Create Event Page</h1>
+      {/* Add your create event form or content here */}
+    </div>
+  );
+};
+
+export default CreateEventPage;
